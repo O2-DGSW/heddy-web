c2d636df0f26dc8ed57de5430b196c3c468bcd00
장강민 <jkm9780533@gmail.com>
2026-05-12 11:11:43 +0900

    refactor: 파일 이름 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './main/index.css'
-import App from './main/App.tsx'
+import './main/global.css'
+import Layout from './main/layout.tsx'
 
 const rootElement = document.getElementById('root')
 
@@ -11,6 +11,6 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <Layout />
   </StrictMode>,
 )
