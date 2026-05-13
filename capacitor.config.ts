d071f2d3af4511e6b5d18d4896eb1c4f493248f3
장강민 <jkm9780533@gmail.com>
2026-05-13 12:06:34 +0900

    chore: Capacitor iOS 시뮬레이터 환경 설정

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.heddy.app',
+  appName: 'heddy',
+  webDir: 'dist/mobile'
+};
+
+export default config;
