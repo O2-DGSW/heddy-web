caf12187a31a0b179c1d38f68598a1150bcf81b4
장강민 <jkm9780533@gmail.com>
2026-05-14 19:54:50 +0900

    refactor: 시뮬레이터 사용시 live로 변경

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
+const isLive = process.env.CAPACITOR_LIVE === 'true';
+
 const config: CapacitorConfig = {
   appId: 'com.heddy.app',
   appName: 'heddy',
-  webDir: 'dist/mobile'
+  webDir: 'dist/mobile',
+  ...(isLive && {
+    server: {
+      url: 'http://localhost:5174',
+      cleartext: true,
+    },
+  }),
 };
 
 export default config;
