22f3e9b735cf323b36a5b1d3948f3fa8cb9effc1
오용준 <dhdydwns723@gmail.com>
2026-05-16 20:37:01 +0900

    refactor: 타입 정리 폴더 생성

--- src/renderer/mobile/src/features/profile/index.ts
+++ src/renderer/mobile/src/features/profile/index.ts
@@ -1,3 +1,3 @@
 export { FieldOfSetting } from "@/features/profile/ui/FieldOfSetting.tsx";
 export { FieldOfTopProfile } from "@/features/profile/ui/FieldOfTopProfile.tsx";
-export type { SettingItemProps } from "@/features/profile/model/SettingItem.types.ts";
+export type { SettingItemProps } from "@/features/profile/model/types/SettingItem.types.ts";
