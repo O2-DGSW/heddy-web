6207231222072eb2340b5192b43727d45d624b30
오용준 <dhdydwns723@gmail.com>
2026-05-16 20:13:18 +0900

    refactor: 타입 정의 분리

--- src/renderer/mobile/src/features/profile/ui/FieldOfSetting.tsx
+++ src/renderer/mobile/src/features/profile/ui/FieldOfSetting.tsx
@@ -1,39 +1,7 @@
 import { font, lightTheme } from "@design-tokens";
-
-import BookmarkIcon from "@/features/profile/assets/setting-field/Bookmark.svg";
-import SettingsIcon from "@/features/profile/assets/setting-field/Setting.svg";
-import PortfolioIcon from "@/features/profile/assets/setting-field/Portfolio.svg";
-import AlarmIcon from "@/features/profile/assets/setting-field/Alarm.svg";
 import ArrowIcon from "@/features/profile/assets/setting-field/Arrow.svg";
-
-interface SettingItemProps {
-  icon: string;
-  alt: string;
-  title: string;
-}
-
-const SETTING_ITEMS = [
-  {
-    icon: BookmarkIcon,
-    alt: "bookmark",
-    title: "저장 스타일",
-  },
-  {
-    icon: PortfolioIcon,
-    alt: "portfolio",
-    title: "포트폴리오 관리",
-  },
-  {
-    icon: SettingsIcon,
-    alt: "setting",
-    title: "설정 관리",
-  },
-  {
-    icon: AlarmIcon,
-    alt: "alarm",
-    title: "알림 설정",
-  },
-];
+import { SETTING_ITEMS } from "@/features/profile/constrants/setting-items.ts";
+import type { SettingItemProps } from "@/features/profile";
 
 const SettingItem = ({ icon, alt, title }: SettingItemProps) => {
   return (
