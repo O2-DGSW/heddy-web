042e05cc531201c385f5b8ad1c33d10a7a44587b
오용준 <dhdydwns723@gmail.com>
2026-05-16 19:47:48 +0900

    feat: 프로필페이지에 ui 요소 적용

--- src/renderer/mobile/src/pages/profile/ui/ProfilePage.tsx
+++ src/renderer/mobile/src/pages/profile/ui/ProfilePage.tsx
@@ -1,9 +1,17 @@
 import { lightTheme } from "@design-tokens";
 
+import { FieldOfTopProfile } from "@/features/profile/ui/FieldOfTopProfile.tsx";
+import { FieldOfSetting } from "@/features/profile/ui/FieldOfSetting.tsx";
+
 export const ProfilePage = () => {
   return (
-    <div className="flex-1" style={{ backgroundColor: lightTheme.fill.normal }}>
-      <div className="h-full">d</div>
+    <div className="flex flex-col h-screen">
+      <div className="h-[31%]">
+        <FieldOfTopProfile />
+      </div>
+      <div className="flex-1" style={{ backgroundColor: lightTheme.fill.normal }}>
+        <FieldOfSetting />
+      </div>
     </div>
   );
 };
