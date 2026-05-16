0a0480d98f82b244b1b9f039b2687a05bbffff44
장강민 <jkm9780533@gmail.com>
2026-05-16 20:51:29 +0900

    rafactor: heddy로고 svg여서 heddyIcon으로 변경

--- src/renderer/mobile/src/pages/login/ui/LoginPage.tsx
+++ src/renderer/mobile/src/pages/login/ui/LoginPage.tsx
@@ -8,7 +8,7 @@ export const LoginPage = () => {
       style={{ backgroundColor: lightTheme.background.normal }}
     >
       <div className="flex flex-col items-center gap-2 mb-14">
-        <img src="/heddy_logo.svg" alt="heddy" className="w-50" />
+        <img src="/heddyIcon.svg" alt="heddy" className="w-50" />
         <p className={font.body.medium} style={{ color: lightTheme.label.assistive }}>
           로그인
         </p>
