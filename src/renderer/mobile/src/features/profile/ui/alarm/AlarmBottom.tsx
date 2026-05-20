822ab58842e67b9e319b460832e00fb8af30ff67
오용준 <dhdydwns723@gmail.com>
2026-05-20 16:12:45 +0900

    refactor: style 로직 개선

--- src/renderer/mobile/src/features/profile/ui/alarm/AlarmBottom.tsx
+++ src/renderer/mobile/src/features/profile/ui/alarm/AlarmBottom.tsx
@@ -3,7 +3,7 @@ import { Toggle } from "@/private/shared/ui/toggle/Toggle.tsx";
 
 export const AlarmBottom = () => {
   return (
-    <div className="size-full px-[1.5rem] py-[3rem]">
+    <div className="size-full px-[1.5rem] py-[2.5rem]">
       <div className="size-full flex flex-col justify-between items-start">
         <p className={font.headline1.semiBold} style={{ color: lightTheme.label.neutral }}>
           쇼핑 혜택 푸시
@@ -18,10 +18,10 @@ export const AlarmBottom = () => {
         <div className="flex flex-row items-center w-full justify-between">
           <div className="flex flex-col items-start">
             <p className={font.body.medium} style={{ color: lightTheme.label.alternative }}>
-              활동/소식
+              혜택/세일/이벤트
             </p>
             <p className={font.label.regular} style={{ color: lightTheme.label.assistive }}>
-              후기 작성, 후기 댓글, 이벤트 참여 등 소식
+              쿠폰, 특가, 관심 상품 가격 인하 등 이벤트 혜택 정보
             </p>
           </div>
           <Toggle />
@@ -29,10 +29,10 @@ export const AlarmBottom = () => {
         <div className="flex flex-row items-center w-full justify-between">
           <div className="flex flex-col items-start">
             <p className={font.body.medium} style={{ color: lightTheme.label.alternative }}>
-              스냅
+              발매
             </p>
             <p className={font.label.regular} style={{ color: lightTheme.label.assistive }}>
-              스냅 좋아요, 댓글, 팔로우 소식 및 패션톡 활동
+              알림 신청한 관심 브랜드 신상품, 인기 상품 재입고
             </p>
           </div>
           <Toggle />
