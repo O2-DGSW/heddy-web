31b6720025d9086f5e8be9523d7bc3f3c8fc9e7d
hyxx-su <legendary9899@dgsw.hs.kr>
2026-05-21 21:14:42 +0900

    feat: 로그인 페이지 router 추가

--- src/renderer/desktop/src/app/routes.tsx
+++ src/renderer/desktop/src/app/routes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { DesktopLayout } from "./layouts";
 import { FeatureNamePage } from "@/pages/featureName";
+import { LoginPage } from "@/pages/login";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<DesktopLayout />}>
         <Route path="/" element={<FeatureNamePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
   );
