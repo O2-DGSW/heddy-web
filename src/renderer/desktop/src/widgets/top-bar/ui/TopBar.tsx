2f711341581df29b6955c69b6db3b994c691db61
hyxx-su <legendary9899@dgsw.hs.kr>
2026-05-21 21:14:26 +0900

    feat: TopBar 스타일 수정

--- src/renderer/desktop/src/widgets/top-bar/ui/TopBar.tsx
+++ src/renderer/desktop/src/widgets/top-bar/ui/TopBar.tsx
@@ -4,7 +4,7 @@ import heddyLogo from "@/widgets/top-bar/assets/svg/heddy-logo.svg";
 
 const TopBar = () => {
   return (
-    <header className="flex h-[88px] items-start px-[104px] pt-[28px]">
+    <header className="flex h-[72px] items-start px-5 pt-5 sm:px-10 lg:px-20">
       <Link to="/" aria-label="Heddy home">
         <img src={heddyLogo} alt="heddy" className="h-8 w-[98px]" />
       </Link>
