299c592b3c351136204690beeb5600eb6ae661b3
장강민 <jkm9780533@gmail.com>
2026-05-31 06:23:28 +0900

    feat: CutsTag에 selected prop 추가

--- src/renderer/mobile/src/private/shared/ui/cuts-tag/CutsTag.tsx
+++ src/renderer/mobile/src/private/shared/ui/cuts-tag/CutsTag.tsx
@@ -2,15 +2,21 @@ import { font, lightTheme } from "@design-tokens";
 
 interface CutsTagProps {
   text: string;
+  selected?: boolean;
 }
 
-export const CutsTag = ({ text }: CutsTagProps) => {
+export const CutsTag = ({ text, selected = false }: CutsTagProps) => {
   return (
     <div
       className="px-[0.325rem] py-[0.125rem] rounded-[0.325rem]"
-      style={{ backgroundColor: lightTheme.fill.neutral }}
+      style={{
+        backgroundColor: selected ? lightTheme.primary.normal : lightTheme.fill.neutral,
+      }}
     >
-      <p className={font.caption.medium} style={{ color: lightTheme.label.alternative }}>
+      <p
+        className={font.caption.medium}
+        style={{ color: selected ? lightTheme.label.buttonText : lightTheme.label.alternative }}
+      >
         # {text}
       </p>
     </div>
