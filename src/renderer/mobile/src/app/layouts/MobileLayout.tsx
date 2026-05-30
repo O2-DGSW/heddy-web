1b4e44a9a72707fa8c74d1c1d5ffcf64e587cfcd
hyxx-su <legendary9899@dgsw.hs.kr>
2026-05-30 21:43:57 +0900

    fix: 레이아웃의 최소 높이를 수정

--- src/renderer/mobile/src/app/layouts/MobileLayout.tsx
+++ src/renderer/mobile/src/app/layouts/MobileLayout.tsx
@@ -8,8 +8,8 @@ const MobileLayout = () => {
     ["/login", "/signup"].includes(location.pathname) || location.pathname.startsWith("/find/");
 
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center">
-      <div className="w-full sm:w-[430px] bg-white min-h-screen relative flex flex-col sm:border-x sm:border-gray-200 sm:shadow-[0_0_24px_rgba(0,0,0,0.05)] transform-gpu overflow-hidden">
+    <div className="min-h-dvh bg-gray-100 flex justify-center">
+      <div className="w-full sm:w-[430px] bg-white min-h-dvh relative flex flex-col sm:border-x sm:border-gray-200 sm:shadow-[0_0_24px_rgba(0,0,0,0.05)] transform-gpu overflow-hidden">
         <main
           className={`flex-1 overflow-y-auto no-scrollbar pt-safe px-safe ${hideBottomBar ? "pb-safe" : "pb-[100px]"}`}
         >
