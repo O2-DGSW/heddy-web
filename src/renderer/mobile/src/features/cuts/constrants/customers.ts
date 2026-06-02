1119a4a48aff91a9ab49c8a457efe1398c92e09b
장강민 <jkm9780533@gmail.com>
2026-06-02 22:36:28 +0900

    feat: Customer 타입 및 더미 데이터 추가

---
+++ src/renderer/mobile/src/features/cuts/constrants/customers.ts
@@ -0,0 +1,9 @@
+import type { Customer } from "@/features/cuts/model/types/Customer.types";
+
+export const DUMMY_CUSTOMERS: Customer[] = [
+  { id: "1", name: "오용준", lastVisitDate: "10/10", lastVisitTime: "10:00", tags: ["남자", "다운펌"] },
+  { id: "2", name: "오용준", lastVisitDate: "10/10", lastVisitTime: "10:00", tags: ["남자", "다운펌"] },
+  { id: "3", name: "오용준", lastVisitDate: "10/10", lastVisitTime: "10:00", tags: ["남자", "다운펌"] },
+  { id: "4", name: "오용준", lastVisitDate: "10/10", lastVisitTime: "10:00", tags: ["남자", "다운펌"] },
+  { id: "5", name: "오용준", lastVisitDate: "10/10", lastVisitTime: "10:00", tags: ["남자", "다운펌"] },
+];
