19dc5dca0dddbaf8b06b4b2c3ba87623ae114416
오용준 <dhdydwns723@gmail.com>
2026-06-07 01:55:33 +0900

    feat: 시술기록 미존재 렌더 페이지 제작

--- src/renderer/mobile/src/features/cuts/ui/PublicSettings.tsx
+++ src/renderer/mobile/src/features/cuts/ui/PublicSettings.tsx
@@ -1,31 +1,14 @@
 // 공개 설정 UI
 import { font, lightTheme } from "@design-tokens";
-import agerSadSvg from "@/features/cuts/assets/procedute-note/agerSad.svg";
 import { usePublicSettings } from "@/features/cuts/model/usePublicSettings.ts";
 import { PublicSettingsCutsItem } from "@/features/cuts/ui/item/PublicSettigsCutsItem.tsx";
+import { NotfoundCutsList } from "@/features/cuts/ui/NotfoundCutsList.tsx";
 
 export const PublicSettings = () => {
   const { notes, handleTogglePublicSettings, handleSave, isDirty } = usePublicSettings();
 
   if (notes.length === 0) {
-    return (
-      <div
-        className="h-full flex flex-col items-center justify-center"
-        style={{ backgroundColor: lightTheme.fill.normal }}
-      >
-        <div className="flex flex-col items-center gap-3">
-          <img src={agerSadSvg} alt="시술기록 없음" className="w-32 h-32" />
-          <p
-            className={`text-center ${font.body.regular}`}
-            style={{ color: lightTheme.label.assistive }}
-          >
-            시술기록이
-            <br />
-            존재하지 않아요..
-          </p>
-        </div>
-      </div>
-    );
+    return <NotfoundCutsList />;
   }
 
   return (
