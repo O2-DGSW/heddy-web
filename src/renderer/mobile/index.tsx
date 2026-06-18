3e825b49675fded4e9aa51c61aa580913ec41f72
오용준 <dhdydwns723@gmail.com>
2026-06-18 22:59:02 +0900

    fix: query-client-provider 활성화 및 tsconfig

--- src/renderer/mobile/index.tsx
+++ src/renderer/mobile/index.tsx
@@ -1,16 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '../../main/global.css'
-import App from './src/app/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../../main/global.css";
+import App from "./src/app/App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-  throw new Error('Root element "#root" was not found.')
+  throw new Error('Root element "#root" was not found.');
 }
+
+const queryClient = new QueryClient();
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
