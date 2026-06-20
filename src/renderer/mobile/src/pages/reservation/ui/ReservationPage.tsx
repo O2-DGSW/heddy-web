b5faa2deb2f947c636dd0463e061254581f1a3a2
오용준 <dhdydwns723@gmail.com>
2026-06-20 19:03:22 +0900

    feat: 날짜 선택 ui 제작

--- src/renderer/mobile/src/pages/reservation/ui/ReservationPage.tsx
+++ src/renderer/mobile/src/pages/reservation/ui/ReservationPage.tsx
@@ -3,31 +3,44 @@ import { Calendar } from "@/shared/ui/calendar";
 import { useState } from "react";
 import {
   DEFAULT_SHOP_SCHEDULE_DATE,
-  SHOP_SCHEDULE_MARKER_MAP,
   SHOP_SCHEDULE_WEEK_DAYS,
-} from "@/features/shop/constrants/schedule-calendar.ts";
+} from "@/features/reservation/constants/schedule-calander.ts";
 
 export const ReservationPage = () => {
   const [selectedDate, setSelectedDate] = useState(DEFAULT_SHOP_SCHEDULE_DATE);
 
   return (
     <>
+      {/* 헤더 */}
       <p
         className={`${font.headline1.bold} text-center py-[1rem]`}
         style={{ color: lightTheme.label.neutral }}
       >
         예약하기
       </p>
-      <Calendar
-        initialMonthDate={DEFAULT_SHOP_SCHEDULE_DATE}
-        markerMap={SHOP_SCHEDULE_MARKER_MAP}
-        selectedDate={selectedDate}
-        variant="mobile"
-        viewMode="week"
-        weekDays={SHOP_SCHEDULE_WEEK_DAYS}
-        className="mx-auto max-w-[393px]"
-        onSelectDate={setSelectedDate}
-      />
+
+      {/* 날짜 선택 */}
+      <div className={"flex flex-col items-center gap-[1rem] w-full px-[1rem]"}>
+        <div className={"flex flex-row items-center justify-between w-full"}>
+          <p className={font.headline2.bold} style={{ color: lightTheme.label.neutral }}>
+            날짜 선택
+          </p>
+          <div />
+        </div>
+        <div className="w-full h-[1.5px]" style={{ backgroundColor: lightTheme.fill.neutral }} />
+        <Calendar
+          initialMonthDate={DEFAULT_SHOP_SCHEDULE_DATE}
+          selectedDate={selectedDate}
+          variant="mobile"
+          viewMode="week"
+          weekDays={SHOP_SCHEDULE_WEEK_DAYS}
+          className="mx-auto max-w-[393px]"
+          onSelectDate={setSelectedDate}
+        />
+      </div>
+
+      {/* 예약 정보 */}
+      <div></div>
     </>
   );
 };
