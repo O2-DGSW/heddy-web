300a5e3523e32f0669238f042f9fe5c707a04e6b
오용준 <dhdydwns723@gmail.com>
2026-07-06 11:23:34 +0900

    fix: api 코드 수정

--- src/renderer/mobile/src/entities/reservation/api/reservationApi.ts
+++ src/renderer/mobile/src/entities/reservation/api/reservationApi.ts
@@ -1,7 +1,12 @@
 import { api, type ApiResponse } from "@heddy/api";
+import type {
+  ReservationRequest,
+  ReservationResponse,
+} from "@/entities/reservation/model/Reservation.types.ts";
 
 export const reservationApi = {
-  postReservaition: async () => {
-    const result = await api.post<ApiResponse<>>();
+  // 매개변수로 ReservationRequest 타입을 받아서 body로 넘겨주도록 수정했습니다.
+  postReservation: async (body: ReservationRequest) => {
+    return await api.post<ApiResponse<ReservationResponse>>(`/treatment-records/register`, body);
   },
 };
--- src/renderer/mobile/src/entities/reservation/model/Reservation.types.ts
+++ src/renderer/mobile/src/entities/reservation/model/Reservation.types.ts
@@ -1,7 +1,31 @@
 export interface ReservationRequest {
   shop_id: number;
-  designer_id: number;
+  designerId: number;
   reserved_at: string;
-  service_tag: string[];
+  service_tags: string[];
   memo: string;
+}
+
+export interface NoShowPrediction {
+  no_show_score: number;
+  risk_level: string;
+  no_show_probability: number;
+  model_version: string;
+}
+
+export interface ReservationResponse {
+  reservation_id: number;
+  customer_id: number;
+  customer_phone_number: string;
+  shop_id: number;
+  shop_name: string;
+  designer_id: number;
+  designer_name: string;
+  reserved_at: string; // ISO 8601 날짜 문자열
+  changed_time: string; // ISO 8601 날짜 문자열
+  service_tags: string[];
+  status: string;
+  memo: string;
+  created_at: string; // ISO 8601 날짜 문자열
+  no_show_prediction: NoShowPrediction;
 }
